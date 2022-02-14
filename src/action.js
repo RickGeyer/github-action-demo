4d362c72d222b5c3e02ac077c75b2b3195b4e30c
Rick Geyer <66496679+RickGeyer@users.noreply.github.com>
2022-02-14 13:58:42 -0600

    added GitHub Actions

--- src/action.js
+++ src/action.js
@@ -1,5 +1,14 @@
+const core = request('@actions/core');
+const github = require('@actions/github');
+
 const run = async() => {
+    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+    const octokit = github.getOctokit(GITHUB_TOKEN);
+
+    const { context = {} } = github;
+    const { pull_request } = context.payload;
     console.log('It is working!')
+    console.dir( pull_request );
 }
 
 run();
